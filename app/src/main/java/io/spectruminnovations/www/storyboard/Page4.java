b630cb88d962d2236cca2e7665d392a9c2af0df5
gregorykielian <gregory.kielian@gmail.com>
2015-02-21 08:30:19 -0800

    Added Page 4 (SMS Prep) adn pg5 placeholder

--- app/src/main/java/io/spectruminnovations/www/storyboard/Page4.java
+++ app/src/main/java/io/spectruminnovations/www/storyboard/Page4.java
@@ -1,9 +1,14 @@
 package io.spectruminnovations.www.storyboard;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Page4 extends ActionBarActivity {
@@ -12,7 +17,39 @@ public class Page4 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page4);
-    }
+
+
+        Button an_sms_button = (Button) findViewById(R.id.page_4_sms_button);
+
+        an_sms_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // write something to do if clicked below
+
+                String phoneNumber = "6505551234"; //Modify this, areacode & number, no spaces
+                String smsMessage = "Hello from my app : )";
+
+                Uri myUri = Uri.parse("sms:" + phoneNumber);
+                Intent intent = new Intent(Intent.ACTION_SENDTO, myUri);
+                intent.putExtra("sms_body", smsMessage);
+                startActivity(intent);
+
+            }
+
+        });
+
+        Button next_page_button = (Button) findViewById(R.id.goto_page_5_button);
+
+        next_page_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // write something to do if clicked below
+
+                Intent intent = new Intent(Page4.this, Page5.class);
+                startActivity(intent);
+
+            }
+
+        });
+}
 
 
     @Override
